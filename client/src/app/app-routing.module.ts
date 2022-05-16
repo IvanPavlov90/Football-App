23e0a108dd96d9b00967a6fb4c32c020703ce4dc
IvanPavlov90 <59957767+IvanPavlov90@users.noreply.github.com>
2022-05-16 12:36:05 +0400

    добавлен роутинг и интерфейс для админ-панели

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { LeaguesComponent } from './leagues/leagues.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LeaguesComponent },
+  { path: 'admin', component: AdminComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeaguesComponent } from './leagues/leagues.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeaguesComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
